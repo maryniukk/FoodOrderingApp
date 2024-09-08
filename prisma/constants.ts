69a720f8cf460a11bcd8fb1d0e4b22fcd224ed4f
Nikita Maryniuk <nikitamaryniuk@Nikitas-MacBook-Pro.local>
2024-09-08 15:40:22 +0200

    Seed.ts is done for using

---
+++ prisma/constants.ts
@@ -0,0 +1,11 @@
+export const categories = [
+	{ name: 'Pizza' },
+	{ name: 'Breakfast' },
+	{ name: 'Snacks' },
+	{ name: 'Cocktails' },
+	{ name: 'Drinks' },
+	{ name: 'Desserts' },
+	{ name: 'Salads' },
+	{ name: 'Soup' },
+	{ name: 'Pasta' },
+];
---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { prisma } from './prisma-client';
+import { hashSync } from 'bcryptjs';
+async function up() {
+	await prisma.user.createMany({
+		data: [
+			{
+				fullName: 'Admin Admin',
+				email: 'admin@admin.com',
+				password: hashSync('admin', 10),
+				role: 'ADMIN',
+				verified: true,
+			},
+			{
+				fullName: 'User User',
+				email: 'user@user.com',
+				password: hashSync('111111', 10),
+				verified: true,
+			},
+		],
+	});
+}
+async function down() {
+	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+async function main() {
+	try {
+		await down();
+		await up();
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
