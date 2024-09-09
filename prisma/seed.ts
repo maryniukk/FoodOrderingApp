2aa7ca5cb341c70bcd1156e30450a98daa47cb4f
Nikita Maryniuk <nikitamaryniuk@Nikitas-MBP.home>
2024-09-09 11:28:15 +0200

    interim commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
-import { categories } from './constants';
+import { categories, ingredients, products } from './constants';
 import { prisma } from './prisma-client';
 import { hashSync } from 'bcryptjs';
+
+function randomNumber(min: number, max: number) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 async function up() {
 	await prisma.user.createMany({
@@ -25,7 +29,63 @@ async function up() {
 	await prisma.category.createMany({
 		data: categories,
 	});
-}
+	await prisma.ingredient.createMany({
+		data: ingredients.map((ingredient) => ({
+			...ingredient,
+			productId: 1,
+		})),
+	});
+
+	await prisma.product.createMany({
+		data: products,
+	});
+
+	const pizza1 = await prisma.product.create({
+		data: {
+			name: 'Пепперони фреш',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+			categoryId: 1,
+			ingredients: {
+				 ingredients.slice(0, 5),
+			},
+		},
+	});
+	const pizza2 = await prisma.product.create({
+		data: {
+			name: 'Сырная',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: ingredients.slice(5, 10),
+			},
+		},
+	});
+	const pizza3 = await prisma.product.create({
+		data: {
+			name: 'Чоризо фреш',
+			imageUrl:
+				'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+			categoryId: 1,
+			ingredients: {
+				connect: ingredients.slice(10, 40),
+			},
+		},
+	});
+
+	// Создаем вариации теста, размера для пиццы
+	await prisma.productItem.createMany({
+		data: [
+			// Первая вариация
+			{
+					productId: pizza1.id,
+					pizzaType: 1,
+					price: randomNumber(7, 11),
+					size: 20,
+			},
+		]
+	})
 
 async function down() {
 	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
@@ -49,4 +109,5 @@ main()
 		console.error(e);
 		await prisma.$disconnect();
 		process.exit(1);
-	});
+	})
+};
